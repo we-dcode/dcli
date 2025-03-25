52c8a708ee442b0db97face291d4e790179bfd43
Maor Davidov <maor@dcode.tech>
2025-03-26 00:42:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 VERSION="${1}"
 
 if [ -z "$VERSION" ]; then
-  VERSION=$(curl -s https://api.github.com/repos/dcode-tech/dcli/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+  VERSION=$(curl -s https://api.github.com/repos/we-dcode/dcli/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 fi
 
 OS="$(uname | tr '[:upper:]' '[:lower:]')"
@@ -14,7 +14,7 @@ ARCH="$(uname -m)"
 if [[ "$ARCH" == "x86_64" ]]; then ARCH="amd64"; fi
 if [[ "$ARCH" == "arm64" || "$ARCH" == "aarch64" ]]; then ARCH="arm64"; fi
 
-URL="https://github.com/dcode-tech/dcli/releases/download/$VERSION/dcli_${VERSION}_${OS}_${ARCH}.tar.gz"
+URL="https://github.com/we-dcode/dcli/releases/download/$VERSION/dcli_${VERSION}_${OS}_${ARCH}.tar.gz"
 
 echo "📦 Downloading dcli $VERSION for $OS/$ARCH..."
 mkdir -p ~/.dcode
